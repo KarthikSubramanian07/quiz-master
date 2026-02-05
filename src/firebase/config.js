cc599d10d5ea0419ad579fd65804595625217a62
Karthik Subramanian <winnerkarthik07@gmail.com>
2026-02-05 13:45:05 -0800

    Configure Firebase with project credentials for Google authentication

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -11,14 +11,17 @@ import { initializeApp } from 'firebase/app'
 import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
+
+
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
-}
+  apiKey: "AIzaSyCLwDxlDJc494KAozBxxkKt3_50ZqgGiF0",
+  authDomain: "quiz-master-6db19.firebaseapp.com",
+  projectId: "quiz-master-6db19",
+  storageBucket: "quiz-master-6db19.firebasestorage.app",
+  messagingSenderId: "764008232044",
+  appId: "1:764008232044:web:2eacb954957d251e421449",
+  measurementId: "G-DP9C6BQPSY"
+};
 
 // Initialize Firebase variables
 let auth = null
